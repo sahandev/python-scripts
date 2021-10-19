a290a55c5b2ed3a4669a90e93052a97e8782ec39
sahandev <sahanxrayn@gmail.com>
2021-10-19 11:20:23 +0530

    add replace name tuple

--- mypy.py
+++ mypy.py
@@ -1,5 +1,14 @@
+
 name = input("enter your name: ")
 age = input("enter your age: ")
 
 print("Hello " +name+ " \n your age is "+age)
 print("WELCOME TO THE SYSTEM "+name.upper())
+
+
+friends= ["sahan", "jazz", "lak"]
+print(friends)
+
+new_friend = input("enter another name to replace tupple 2nd name: ")
+friends [2] = new_friend
+print(friends)

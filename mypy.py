f0dde9f9ddb55ed8925a2d410165dcf56dc5febe
sahandev <sahanxrayn@gmail.com>
2021-10-19 11:29:35 +0530

    recorrect "tupple " to "List"

--- mypy.py
+++ mypy.py
@@ -5,10 +5,10 @@ age = input("enter your age: ")
 print("Hello " +name+ " \n your age is "+age)
 print("WELCOME TO THE SYSTEM "+name.upper())
 
-##replace tuple value from user input
+##replace list value from user input
 friends= ["sahan", "jazz", "lak"]
 print(friends)
 
-new_friend = input("enter another name to replace tupple 2nd name: ")
+new_friend = input("enter another name to replace list 2nd name: ")
 friends [2] = new_friend
 print(friends)
